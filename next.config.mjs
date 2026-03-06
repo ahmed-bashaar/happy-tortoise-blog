cec029f436540b0b7ea1d7d3d42b7abff1406a13
Ahmed Elbashaar <ahmed.bashaar@outlook.com>
2026-03-06 04:16:03 +0200

    Switched from config from Webpack to Turbopack

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,14 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
-  webpack: (webpackConfig) => {
-    webpackConfig.resolve.extensionAlias = {
+  turbopack: (config) => {
+    config.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
       '.js': ['.ts', '.tsx', '.js', '.jsx'],
       '.mjs': ['.mts', '.mjs'],
     }
 
-    return webpackConfig
+    return config
   },
 }
 
